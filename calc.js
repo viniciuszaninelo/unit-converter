5f2efe8353e7f85d6d37d9988458291d0be49b41
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2021-11-27 19:51:53 -0300

    comit

---
+++ calc.js
@@ -0,0 +1,9 @@
+
+
+  $(function(){
+    $('#calc').click(function(){
+      const milhas = parseFloat($('#milhas').val())
+      const metros = 1609.34 * milhas
+      $('#metros').val(metros)
+    })
+  })
